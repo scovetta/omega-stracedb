d303b5f1fe4993788bd9e4b63eca064723ab28f3
Michael Scovetta <michael.scovetta@gmail.com>
2022-07-18 11:27:53 -0700

    Update execution for new path spec

--- src/Run.ps1
+++ src/Run.ps1
@@ -17,7 +17,19 @@ param (
 
     [Parameter(Mandatory=$false)]
     [string]
-    $AptMirrorPath
+    $AptMirrorPath,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $PackageVersion,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $ALWAYS_INSTALL = "",
+
+    [Parameter(Mandatory=$false)]
+    [bool]
+    $ForceAnalyze = $false
 )
 
 # Copyright Linux Foundation and contributors. Licensed under the MIT license.
@@ -34,8 +46,22 @@ $RetryCount = 2
 
 $IMAGE_TAG = "latest"
 
-if (Test-Path -Type Container (Join-Path $ResultsDirectory $PackageName)) {
-    Write-Host "Package [$PackageName] already analyzed."
+if (!$ForceAnalyze) {
+    $PackageNameLower = $PackageName.ToLower()
+    if ($PackageNameLower.Length -eq 1) {
+        $ExportPrefix = $PackageNameLower[0]
+    } elseif ($PackageNameLower.Length -gt 1) {
+        $ExportPrefix = (Join-Path $PackageNameLower[0] $PackageNameLower.Substring(0, 2))
+    } else {
+        $ExportPrefix = "_"
+    }
+    $FinalDirectory = (Join-Path (Join-Path $ResultsDirectory $ExportPrefix) $PackageName)
+    Write-Output $FinalDirectory
+
+    if (Test-Path -Type Container $FinalDirectory) {
+        Write-Host "Package [$PackageName] already analyzed."
+        Exit 0
+    }
     Exit 0
 }
 
@@ -51,10 +77,10 @@ while ($RetryCount -gt 0)
         $APT_MIRROR_PATH = $(wsl pwd)
         cd $CURRENT_DIRECTORY
 
-        & $DOCKER_CMD $DOCKER_ARG run --rm -it --mount type=bind,source=$APT_MIRROR_PATH,target=/opt/apt-mirror,readonly --mount type=bind,source=$RESULTS_REPOSITORY_WSL,target=/opt/export openssf/omega-tracer:$IMAGE_TAG $PackageName $InstallCommand
+        & $DOCKER_CMD $DOCKER_ARG run --rm -it $ALWAYS_INSTALL --mount type=bind,source=$APT_MIRROR_PATH,target=/opt/apt-mirror,readonly --mount type=bind,source=$RESULTS_REPOSITORY_WSL,target=/opt/export openssf/omega-tracer:$IMAGE_TAG $PackageName $InstallCommand $PackageVersion
     } else {
         $APT_MIRROR_ARG = ""
-        & $DOCKER_CMD $DOCKER_ARG run --rm -it --mount type=bind,source=$RESULTS_REPOSITORY_WSL,target=/opt/export openssf/omega-tracer:$IMAGE_TAG $PackageName $InstallCommand
+        & $DOCKER_CMD $DOCKER_ARG run --rm -it $ALWAYS_INSTALL --mount type=bind,source=$RESULTS_REPOSITORY_WSL,target=/opt/export openssf/omega-tracer:$IMAGE_TAG $PackageName $InstallCommand $PackageVersion
     }
 
     $RESULT=$LASTEXITCODE
